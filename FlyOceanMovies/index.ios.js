85929fe78ce103a66ee5f710c0c239832bbe1d8b
FlyOceanFish <wangrifei1@126.com>
2017-09-18 17:44:01 +0800

    T提交

--- FlyOceanMovies/index.ios.js
+++ FlyOceanMovies/index.ios.js
@@ -4,50 +4,104 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    View,
+    FlatList,
+    Image,
+    Text
 } from 'react-native';
 
 export default class FlyOceanMovies extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+        this.fetchData();
+    }
+
+    _keyExtractor = (item, index) => item.id;
+
+    fetchData() {
+        fetch('https://api.douban.com/v2/movie/top250')
+            .then(response => response.json())
+            .then(responseJson => {
+                console.log('response-----：\n' + JSON.stringify(responseJson));
+                this.setState({
+                    data: responseJson.subjects
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    _renderItem = (movie) => (
+        <View style={styles.item}>
+            <View style={styles.itemImage}>
+                <Image source={{uri: movie.images}} style={styles.image}/>
+            </View>
+            <View style={styles.itemContent}>
+                <Text style={styles.itemHeader}>{movie.title}</Text>
+                <Text style={styles.meta}>{movie.original_title}({movie.year})</Text>
+                <Text style={styles.redText}>{movie.rating}</Text>
+
+            </View>
+        </View>
+
     );
-  }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={this._renderItem}
+                    keyExtractor={this._keyExtractor}
+                    style={styles.container}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffb40b',
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        fontSize: 15,
+        marginBottom: 5,
+    },
+    item: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(100,253,201,0.1)',
+        flex: 1,
+        marginBottom: 6
+    },
+    itemImage: {
+        flex: 1,
+        width: 50,
+        height: 70
+    },
+    itemContent: {
+        flex: 1,
+        marginLeft: 13,
+        marginTop: 6,
+    },
+    itemHeader: {
+        fontSize: 18,
+        fontFamily: 'Helvetica Neue',
+        fontWeight: '300',
+        color: '#6435c9',
+        marginBottom: 6
+    },
+    redText: {}
 });
 
 AppRegistry.registerComponent('FlyOceanMovies', () => FlyOceanMovies);
