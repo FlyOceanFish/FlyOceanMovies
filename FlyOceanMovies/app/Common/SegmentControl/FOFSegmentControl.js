4bead4145c4ff4520720b335ced325b3e0d6d3fe
OceanFish <wangrifei1@126.com>
2017-10-23 22:15:13 +0800

    重构

--- FlyOceanMovies/app/Common/SegmentControl/FOFSegmentControl.js
+++ FlyOceanMovies/app/Common/SegmentControl/FOFSegmentControl.js
@@ -5,60 +5,36 @@ import {
     TouchableHighlight,
     Animated,
     Text,
-    Dimensions,
     StyleSheet,
 } from 'react-native';
 
 import PropTypes from 'prop-types';
 
-const SCROLL_BAR_WIDTH = 70;
-var preIndex = 0;
+var width;
+
 export default class FOFSegmentControl extends Component{
     constructor(props){
         super(props);
-
-
         this.state = {
-            offXItem:0,
             translateValue:new Animated.ValueXY({x:0,y:0})
         };
     }
-
-    componentDidMount() {
-        // this.refs.parentView.measure((x,y,width,height,pageX,pageY)=>{//此时调用全部返回0，所以不适用此时调用
-        //     offXItem = (width/3.0-SCROLL_BAR_WIDTH)/2.0;
-        //     console.log(x,y,width,pageX,pageY,height);
-        // })
-    }
-    _startRightAnimation(i) {
-        Animated.spring(this.state.translateValue,{
-            toValue:{x:this.state.offXItem*2*i+SCROLL_BAR_WIDTH*i, y:0},
-            friction: 5,// 摩擦力，默认为7.
-            tension: 15,// 张力，默认40。
-        }).start();
-    }
-    _startLeftAnimation(i) {
-        Animated.spring(this.state.translateValue,{
-            toValue:{x:this.state.offXItem*2*i+SCROLL_BAR_WIDTH*i, y:0},
-            friction: 5,// 摩擦力，默认为7.
-            tension: 15,// 张力，默认40。
-        }).start();
-    }
     _startAnimation(i){
-        if (preIndex<i){
-            this._startRightAnimation(i);
-        }else {
-            this._startLeftAnimation(i);
-        }
+        Animated.spring(this.state.translateValue,{
+            toValue:{x:this._offX(i), y:0},
+            friction: 5,// 摩擦力，默认为7.
+            tension: 15,// 张力，默认40。
+        }).start();
     }
     _renderTitle(title, i) {
         return (
             <View style={styles.title}>
-                {/*<Text style={[this.props.titleStyle, i === this.props.index && this.props.selectedTitleStyle]}>{title}</Text>*/}
-                <Text style={this.props.titleStyle}>{title}</Text>
-
+                <Text style={[this.props.titleStyle, i === this.props.selectedIndex && this.props.selectedTitleStyle]}>{title}</Text>
             </View>
         );
+    }
+    _offX(i){
+        return (width/this.props.titles.length-this.props.barWidth)/2.0+width/this.props.titles.length*i;
     }
     renderTitle(title, i) {
         return (
@@ -79,18 +55,17 @@ export default class FOFSegmentControl extends Component{
             items.push(this.renderTitle(titles[i],i));
         }
         let scrollBar = (
-            <Animated.View ref='scrollBar' style={[styles.scrollBar,{opacity:0,backgroundColor:this.props.barColor,marginLeft:this.state.offXItem,transform:[
+            <Animated.View ref='scrollBar' style={[styles.scrollBar,{width:this.props.barWidth,backgroundColor:this.props.barColor,transform:[
                 {translateX:this.state.translateValue.x},
                 {translateY:this.state.translateValue.y}
             ]}]}/>
         )
         return(
             <View ref='parentView' {...this.props} style={[styles.container,this.props.style]}  onLayout={(event)=>{
-                let width = event.nativeEvent.layout.width;
-                this.refs.scrollBar.setNativeProps({//也可以使用setState来实现
-                    style:{opacity:1}
-                })
-                this.setState({offXItem:(width/3.0-SCROLL_BAR_WIDTH)/2.0});
+                width = event.nativeEvent.layout.width;
+                this.setState({
+                    translateValue:new Animated.ValueXY({x:this._offX(this.props.selectedIndex),y:0})
+                });
             }}>
                 {this.props.barPosition=='top' && scrollBar}
                 <View style={styles.titleContainer}>
@@ -105,13 +80,13 @@ FOFSegmentControl.prototypes = {
     titles:PropTypes.array,
     titleStyle:PropTypes.style,
     selectedTitleStyle:PropTypes.style,
-    barColor:PropTypes.string,
     onPress:PropTypes.func,
     selectedIndex:PropTypes.number,
     renderTitle:PropTypes.func,
     underlayColor:PropTypes.string,
     barPosition:PropTypes.string,
-    barColor:PropTypes.string
+    barColor:PropTypes.string,
+    barWidth:PropTypes.number
 };
 
 FOFSegmentControl.defaultProps = {
@@ -124,7 +99,8 @@ FOFSegmentControl.defaultProps = {
     selectedIndex:0,
     underlayColor:'transparent',
     barPosition:'bottom',
-    barColor:'red'
+    barColor:'red',
+    barWidth:70
 };
 
 var styles = StyleSheet.create({
@@ -141,6 +117,5 @@ var styles = StyleSheet.create({
     },
     scrollBar:{
         height:1,
-        width:SCROLL_BAR_WIDTH,
     },
 });
